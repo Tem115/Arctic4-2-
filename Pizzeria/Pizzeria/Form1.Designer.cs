837e226aea036695be5fd425436cae9eff4d68af
Tem115 <37000158+Tem115@users.noreply.github.com>
2018-04-26 11:22:27 +0500

    q

---
+++ Pizzeria/Pizzeria/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Pizzeria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 5, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 34);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(656, 299);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "№";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(55, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ф.И.О";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(231, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Адрес";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(372, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Пицца";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(478, 1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Диаметр";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(574, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Соус";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(121, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenToolStripMenuItem,
+            this.SaveToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.OpenToolStripMenuItem.Text = "Открыть";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click_1);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.SaveToolStripMenuItem.Text = "Сохранить";
+            this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click_1);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Новый клиент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить клиента";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(842, 345);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Заказы";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Pizzeria/Pizzeria/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Pizzeria
+{
+    
+    public partial class Form1 : Form
+    {
+        new int Click = 0;
+        public List<Person> Human = new List<Person>();
+        public class Person
+        {
+            public Label Label = new Label();
+            public TextBox FIO = new TextBox();
+            public TextBox Address = new TextBox();
+            public TextBox Pizza = new TextBox();
+            public TextBox Diametr = new TextBox();
+            public CheckBox Sauce = new CheckBox();
+        }
+
+        public List<Treatment> HumanTreat = new List<Treatment>();
+        [Serializable]
+        public class Treatment
+        {
+            public string Label;
+            public string FIO;
+            public string Address;
+            public string Pizza;
+            public string Diametr;
+            public bool Sauce;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "XML files(*.xml)|*.xml";
+            saveFileDialog1.Filter = "XML files(*.xml)|*.xml";  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Click++;
+            Human.Add(new Person());
+            Draw(Click - 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Delete(1);
+        }
+
+        private void Draw(int element)
+        {
+            tableLayoutPanel1.RowCount++;
+            for (int column = 0; column < 6; column++)
+            {
+                tableLayoutPanel1.Controls.Add(Qwaqwa(element, column), column, tableLayoutPanel1.RowCount-2);
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30));
+                tableLayoutPanel1.RowStyles[1+ element].Height = 30;
+                Qwaqwa(element, column).Anchor = (AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left);
+                Qwaqwa(element, column).Font = new System.Drawing.Font("Times New Roman", this.Height / 30);
+            }
+            Human.ElementAt(element).Sauce.CheckAlign = ContentAlignment.MiddleCenter;
+            Human.ElementAt(element).Label.Text = (tableLayoutPanel1.RowCount-2).ToString();
+        }
+        
+        private void Delete(int Row)
+        {
+            for (int i = 0; i < Row; i++)
+            {
+                if (tableLayoutPanel1.RowCount > 2)
+                {
+                    tableLayoutPanel1.RowCount--;
+                    for (int column = 0; column < 6; column++)
+                        tableLayoutPanel1.Controls.Remove(Qwaqwa(Human.Count - 1, column));
+                    Human.RemoveAt(Human.Count - 1);
+                    Click--;
+                }
+            }
+        }
+
+        private void OpenToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            Delete(tableLayoutPanel1.RowCount - 2);
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Treatment>));
+            using (FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate))
+            {
+                HumanTreat = (List<Treatment>)formatter.Deserialize(fs);
+            }
+            Open();
+            HumanTreat.Clear();
+        }
+
+        private void Open()
+        {
+            for (int i = 0; i < HumanTreat.Count; i++)
+            {
+                Human.Add(new Person());
+                Human.ElementAt(i).Label.Text = HumanTreat.ElementAt(i).Label;
+                Human.ElementAt(i).FIO.Text = HumanTreat.ElementAt(i).FIO;
+                Human.ElementAt(i).Address.Text = HumanTreat.ElementAt(i).Address;
+                Human.ElementAt(i).Pizza.Text = HumanTreat.ElementAt(i).Pizza;
+                Human.ElementAt(i).Diametr.Text = HumanTreat.ElementAt(i).Diametr;
+                Human.ElementAt(i).Sauce.Checked = HumanTreat.ElementAt(i).Sauce;
+                Draw(i);
+                Click++;
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Save();
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string fileNameSave = saveFileDialog1.FileName;
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Treatment>));
+            using (FileStream fs = new FileStream(fileNameSave, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, HumanTreat);
+            }
+            HumanTreat.Clear();
+        }
+
+        private void Save()
+        {
+            for (int i = 0; i < Human.Count; i++)
+            {
+                HumanTreat.Add(new Treatment());
+                HumanTreat.ElementAt(i).Label = Human.ElementAt(i).Label.Text;
+                HumanTreat.ElementAt(i).FIO = Human.ElementAt(i).FIO.Text;
+                HumanTreat.ElementAt(i).Address = Human.ElementAt(i).Address.Text;
+                HumanTreat.ElementAt(i).Pizza = Human.ElementAt(i).Pizza.Text;
+                HumanTreat.ElementAt(i).Diametr = Human.ElementAt(i).Diametr.Text;
+                HumanTreat.ElementAt(i).Sauce = Human.ElementAt(i).Sauce.Checked;
+            }
+        }
+
+        private Control Qwaqwa(int i, int column)
+        {
+            switch (column)
+            {
+                case 0:
+                    return Human.ElementAt(i).Label;
+                case 1:
+                    return Human.ElementAt(i).FIO;
+                case 2:
+                    return Human.ElementAt(i).Address;
+                case 3:
+                    return Human.ElementAt(i).Pizza;
+                case 4:
+                    return Human.ElementAt(i).Diametr;
+                default:
+                    return Human.ElementAt(i).Sauce;
+            }
+        }
+    }
+}
